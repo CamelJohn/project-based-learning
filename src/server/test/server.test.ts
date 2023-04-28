654f5a9175e88f374d1bba9b2a2bee3b2ebb81a0
camelJohn <jonathan23986@gmail.com>
2023-04-28 09:09:44 +0300

    refactored middleware tests

--- src/server/test/server.test.ts
+++ src/server/test/server.test.ts
@@ -1,41 +1,30 @@
 import supertest from "supertest";
 import { Server } from "../index";
 
-describe("Server", () => {
+describe("Server Base Middleware", () => {
   let webServer: Awaited<ReturnType<typeof Server>>;
 
   beforeAll(async () => {
     webServer = await Server({ test: true });
   });
+
   afterEach(() => webServer.close());
 
-  describe("GET => /health", () => {
-    it("should return status 200", async () => {
-      const response = await supertest(webServer).get("/health");
-      expect(response.statusCode).toEqual(200);
-      webServer.close();
-    });
-
-    it("should return text Ok", async () => {
+  describe("Health Check Middleware", () => {
+    it("should return status 200 & Ok text", async () => {
       const response = await supertest(webServer).get("/health");
       expect(response.body).toEqual("Ok");
-      webServer.close();
+      expect(response.statusCode).toEqual(200);
     });
   });
 
-  describe("any METHOD => 'unknown_route'", () => {
-    it("should return status 404", async () => {
-      const response = await supertest(webServer).get("/smuckini");
-      expect(response.statusCode).toEqual(404);
-      webServer.close();
-    });
-
-    it("should return a Not-Found Error", async () => {
-      const response = await supertest(webServer).get("/smuckini");
+  describe("Catch All Middleware", () => {
+    it("should return status 404 & Not-Found error message", async () => {
+      const response = await supertest(webServer).get("/lies");
       expect(response.body.message).toEqual(
         "the route you are looking for does not exist."
       );
-      webServer.close();
+      expect(response.statusCode).toEqual(404);
     });
   });
 });
